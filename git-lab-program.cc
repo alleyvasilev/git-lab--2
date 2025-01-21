49574491620c08eceb47ad9073fe60bf96a644b3
alley vasilev <av654823@ohio.edu>
2025-01-21 11:53:50 -0500

    Hello Git program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Alley Vasilev
+ *        Date: 1/21/2025
+ * Description: Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
